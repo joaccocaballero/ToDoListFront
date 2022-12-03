03b749712ddd428800649f240fd30637acf2282a
joaccocaballero <joaquincaballero182@gmail.com>
2022-12-02 22:27:43 -0300

    punto

--- src/models/objectUser/objectUser.js
+++ src/models/objectUser/objectUser.js
@@ -1,8 +1,8 @@
 export class User {
     constructor(firstName,lastName, email, password){
-        this.firstName = firstName,
-        this.lastName = lastName,
-        this.email = email,
-        this.password = password
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
     }
 }
